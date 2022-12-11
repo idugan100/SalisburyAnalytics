3254e9e4311a5daee4487a69783c4ad298accc71
idugan100 <isaacd4444@gmail.com>
2022-12-11 12:01:22 -0500

    added all data veiwing to course index view

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -1,1 +1,11 @@
-<h2>hello index</h2>
+<h1>All Courses</h1>
+@foreach ($courses as $course)
+    <h3>{{$course->departmentCode . "-" . $course->courseNumber}}</h3>
+    <h4>{{$course->creditsTotal . " total credit hours"}}</h4>
+    <p>{{$course->description}}</p>
+    <a href={{$course->syllabusLink}}> Syllabus</a>
+    <p>{{$course->creditsLecture . " hours lecture"}}</p>
+    <p>{{$course->creditsLab . " hours lab"}}</p>
+
+    <hr>
+@endforeach
