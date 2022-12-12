66a8d70e43200d49ded5af62403891f7cd486085
idugan100 <isaacd4444@gmail.com>
2022-12-12 11:47:08 -0500

    boilerplate for professor model

---
+++ app/Http/Controllers/ProfessorController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreProfessorRequest;
+use App\Http\Requests\UpdateProfessorRequest;
+use App\Models\Professor;
+
+class ProfessorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreProfessorRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreProfessorRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Professor  $professor
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Professor $professor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Professor  $professor
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Professor $professor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateProfessorRequest  $request
+     * @param  \App\Models\Professor  $professor
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateProfessorRequest $request, Professor $professor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Professor  $professor
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Professor $professor)
+    {
+        //
+    }
+}
