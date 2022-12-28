4cbb3c55e7b0c0140fc6da610264415155b1e204
idugan100 <isaacd4444@gmail.com>
2022-12-28 16:51:04 -0500

    set syllabus links to open in a new tab

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -3,7 +3,7 @@
     <h3>{{$course->departmentCode . "-" . $course->courseNumber}}</h3>
     <h4>{{$course->creditsTotal . " total credit hours"}}</h4>
     <p>{{$course->description}}</p>
-    <a href={{$course->syllabusLink}}> Syllabus</a>
+    <a href={{$course->syllabusLink}} target="_blank"> Syllabus</a>
     <p>{{$course->creditsLecture . " hours lecture"}}</p>
     <p>{{$course->creditsLab . " hours lab"}}</p>
     <form method="POST" action="{{ route('courses.destroy', $course->id) }}">
