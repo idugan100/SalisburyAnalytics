9a835841df0d66182f64920d118d74de12b40e20
idugan100 <isaacd4444@gmail.com>
2023-01-02 00:51:36 -0500

    professor index page styling

--- resources/views/professors/index.blade.php
+++ resources/views/professors/index.blade.php
@@ -1,11 +1,17 @@
 <x-header>
     <h2 class="bg-yellow-500 text-center font-bold text-white font-xl">All Professors</h2>
     @if (!empty($professors))
+        <div class="flex justify-center border-2">
+            <div>
         @foreach ($professors as $professor)
-            <h3>{{$professor->firstName . " " . $professor->lastName}}</h3>
+           
+                <h3 class="border-2 m-3 text-2xl p-2 max-w-sm">{{$professor->firstName . " " . $professor->lastName . " - " . $professor->department}}</h3>
+
+            
             
         @endforeach
-        
+            </div>
+        </div>
     @endif
 
 </x-header>
