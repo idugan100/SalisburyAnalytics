1d533af385cc8735933771e3952ead4bb7e9cf61
idugan100 <isaacd4444@gmail.com>
2023-01-02 01:34:39 -0500

    about page and nav link

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,8 +1,10 @@
-<nav class="p-2 bg-gray-500 text-white">
+<nav class="p-3 bg-gray-500 text-white">
     <a class="bg-red-900 p-1 rounded" href="{{route("courses.create")}}">New Course</a>
     <a href="{{route("professors.create")}}" class="bg-red-900 p-1 rounded">New Professor</a>
     <a href="{{route("professors.index")}}" class="bg-red-900 p-1 rounded">All Professors</a>
     <a href="{{route("courses.index")}}" class="bg-red-900 p-1 rounded">All Courses</a>
+    <a class="bg-red-900 p-1 rounded" href="/">About</a>
+
 
 
 </nav>
