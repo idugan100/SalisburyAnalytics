f917a3d8ef24940f8d1f461926f3f5f4cbc706f6
idugan100 <isaacd4444@gmail.com>
2023-01-01 22:57:47 -0500

    basic create course styling so it's useable

--- resources/views/courses/create.blade.php
+++ resources/views/courses/create.blade.php
@@ -1,37 +1,39 @@
 <x-header>
     <h2 class="bg-yellow-500 text-center font-bold text-white font-xl">Add Course</h2>
-<form method="POST" action="{{route('courses.store')}}">
+<div class="flex justify-center ">
+<form method="POST" action="{{route('courses.store')}}" >
     @csrf
    
-    <div>
-        <label>Department Code</label>
-        <input name="departmentCode" type="text">
+    <div class="m-4">
+        <label class="mr-2">Department Code</label>
+        <input class="border-2 border-black" name="departmentCode" type="text">
     </div>
-    <div>
-        <label>Course Number</label>
-        <input name="courseNumber" type="text">
+    <div class="m-4">
+        <label class="mr-2">Course Number</label>
+        <input class="border-2 border-black" name="courseNumber" type="text">
     </div>
     <label>Course Description</label>
 
-    <div>
-        <textarea name="description" cols="30" rows="10"></textarea>
+    <div class="m-4">
+        <textarea class="border-2 border-black" name="description" cols="30" rows="10"></textarea>
+    </div> 
+    <div class="m-4">
+        <label class="mr-2">Syllabus Link</label>
+        <input class="border-2 border-black" name="syllabusLink" type="text">
     </div>
-    <div>
-        <label>Syllabus Link</label>
-        <input name="syllabusLink" type="text">
+    <div class="m-4">
+        <label class="mr-2">Lab Hours Per Week</label>
+        <input class="border-2 border-black" name="creditsLab" type="number">
     </div>
-    <div>
-        <label>Lab Hours Per Week</label>
-        <input name="creditsLab" type="number">
+    <div class="m-4">
+        <label class="mr-2">Lecture Hours Per Week</label>
+        <input class="border-2 border-black" name="creditsLecture" type="number">
     </div>
-    <div>
-        <label>Lecture Hours Per Week</label>
-        <input name="creditsLecture" type="number">
+    <div class="m-4">
+        <label class="mr-2">Credit Hours</label>
+        <input class="border-2 border-black" name="creditsTotal" type="number">
     </div>
-    <div>
-        <label>Credit Hours</label>
-        <input name="creditsTotal" type="number">
-    </div>
-    <button type="submit">Submit</button>
+    <button class="bg-gray-200 hover:bg-blue-900 hover:text-white rounded p-1" type="submit">Submit</button>
 </form>
+</div>
 </x-header>
