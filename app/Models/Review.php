c4957939b4ff0e863a321c2b79cb1d91d5ba3262
idugan100 <isaacd4444@gmail.com>
2023-01-02 23:05:03 -0500

    added inverse relationships to review model

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+    protected $fillable = ['question','response','professor_id','course_id'];
+
+    public function professor(){
+        return $this->belongsTo(Professor::class);
+    }
+    public function course(){
+        return $this->belongsTo(Course::class);
+    }
 }
