822eca4d6fc1796c5bc5b0b4148c6a76646d7af8
idugan100 <isaacd4444@gmail.com>
2023-01-06 16:09:58 -0500

    added error display for search validation and cleaned up index code indentation

--- lang/en/validation.php
+++ lang/en/validation.php
@@ -117,7 +117,7 @@ return [
     'prohibited_if' => 'The :attribute field is prohibited when :other is :value.',
     'prohibited_unless' => 'The :attribute field is prohibited unless :other is in :values.',
     'prohibits' => 'The :attribute field prohibits :other from being present.',
-    'regex' => 'The :attribute format is invalid.',
+    'regex' => 'The search format is invalid please refer to the place holder text for a valid format.',
     'required' => 'The :attribute field is required.',
     'required_array_keys' => 'The :attribute field must contain entries for: :values.',
     'required_if' => 'The :attribute field is required when :other is :value.',
