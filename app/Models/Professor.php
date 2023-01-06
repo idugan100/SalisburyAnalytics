d9945d9dc23d8b1172518e2ec5f0a428da04aac6
Isaac-opg <isaacd4444@gmail.com>
2023-01-06 16:47:13 +0000

    added filter scope and integrated with controller

--- app/Models/Professor.php
+++ app/Models/Professor.php
@@ -9,6 +9,26 @@ class Professor extends Model
 {
     use HasFactory;
     protected $fillable=['firstName','lastName','deparment'];
+    
+    public function scopeFilter( $query, $searchTerm){
+        
+        
+        if($searchTerm){
+            
+            $explodedTerms=explode( " ", $searchTerm);
+            $searchedPosts=$query
+                ->where('firstName' , 'LIKE' , "%" . $explodedTerms[0]. "%")
+                ->where('lastName', 'LIKE', "%" . $explodedTerms[1] . "%");
+            return $searchedPosts;
+        }
+        else{
+            dd("else");
+
+            return $query;
+        }
+    }
+
+   
     public function reviews(){
         return $this->hasMany(Review::class);
     }
