2d5c2bc91bf5507b7f7f40afa07f72ed921a6d16
idugan100 <isaacd4444@gmail.com>
2023-01-06 16:09:58 -0500

    added error display for search validation and cleaned up index code indentation

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -1,40 +1,43 @@
 
 <x-header>
+
 <h2 class="bg-yellow-500 text-center font-bold text-white text-xl">All Courses</h2>
+
 <form class="flex justify-center" action="{{route('courses.index')}}">
     @method('get')
     <input placeholder="COSC-120" class="border-2 p-2 border-black " name ="search" type="text">
     <button class="p-2 border-2 border-black hover:bg-black hover:text-white" type="submit">Search</button>
-  </form>
-  @if(count($courses)==0)
+</form>
+
+@error('search')
+    <div class="flex justify-center text-red-800 text-lg">Please enter your search in the following format: DEPT-123</div>
+@enderror
+
+@if(count($courses)==0)
   <p class="flex justify-center m-3">We couldn't find that course. Please double check the department code and course number</p>
-  @endif
-@if (!empty($courses))
+@else (!empty($courses))
     <div class="flex justify-center border" >
-        <div class="max-w-3xl p-6">
-@foreach ($courses as $course)
-    <div class="p-3">
-        <h3 class="text-2xl font-bold underline">{{$course->courseTitle }}</h3>
-        <h3 class="text-2xl font-bold underline">{{$course->departmentCode . "-" . $course->courseNumber}}</h3>
-        <h4>{{$course->creditsTotal . " total credit hours"}}</h4>
-        <p>{{$course->description}}</p>
-        <a class="underline text-blue-800"href={{$course->syllabusLink}} target="_blank"> Syllabus</a>
-        <p>{{$course->creditsLecture . " hours lecture"}}</p>
-        <p>{{$course->creditsLab . " hours lab"}}</p>
+    <div class="max-w-3xl p-6">
+    @foreach ($courses as $course)
+        <div class="p-3">
+            <h3 class="text-2xl font-bold underline">{{$course->courseTitle }}</h3>
+            <h3 class="text-2xl font-bold underline">{{$course->departmentCode . "-" . $course->courseNumber}}</h3>
+            <h4>{{$course->creditsTotal . " total credit hours"}}</h4>
+            <p>{{$course->description}}</p>
+            <a class="underline text-blue-800"href={{$course->syllabusLink}} target="_blank"> Syllabus</a>
+            <p>{{$course->creditsLecture . " hours lecture"}}</p>
+            <p>{{$course->creditsLab . " hours lab"}}</p>
         {{--delete button--}}
-        <form  class="my-2" method="POST" action="{{ route('courses.destroy', $course->id) }}">
-            @csrf
-            @method('delete')
-            <button class="bg-gray-200 hover:bg-red-900 hover:text-white rounded p-1">Delete</button>
-
-        </form>
-        <hr>
+            <form  class="my-2" method="POST" action="{{ route('courses.destroy', $course->id) }}">
+                @csrf
+                @method('delete')
+                <button class="bg-gray-200 hover:bg-red-900 hover:text-white rounded p-1">Delete</button>
+            </form>
+            <hr>
+        </div>
+    @endforeach
     </div>
-    
-@endforeach
-</div>
-</div>
+    </div>
 @endif
-
 
 </x-header>
--- resources/views/professors/index.blade.php
+++ resources/views/professors/index.blade.php
@@ -1,31 +1,35 @@
 <x-header>
-    <h2 class="bg-yellow-500 text-center font-bold text-white text-xl">All Professors</h2>
-  <form class="flex justify-center" action="{{route('professors.index')}}">
+
+<h2 class="bg-yellow-500 text-center font-bold text-white text-xl">All Professors</h2>
+
+<form class="flex justify-center" action="{{route('professors.index')}}">
     @method('get')
     <input placeholder ="John Smith" class="border-2 p-2 border-black " name ="search" type="text">
     <button class="p-2 border-2 border-black hover:bg-black hover:text-white" type="submit">Search</button>
-  </form>
-    @if (!empty($professors))
-        <div class="flex justify-center ">
-            <div >
-        @foreach ($professors as $professor)
-                <div class="m-3 border-2 flex shadow-md">
-                <h3 class=" text-2xl p-2 max-w-sm">{{$professor->firstName . " " . $professor->lastName . " - " . $professor->department}}</h3>
-                <form  class="m-2" method="POST" action="{{ route('professors.destroy', $professor->id) }}">
-                    @csrf
-                    @method('delete')
-                    <button class="bg-gray-200 hover:bg-red-900 hover:text-white rounded p-1">Delete</button>
-        
-                </form>
-             
-                <a class="m-2 bg-gray-200 hover:bg-blue-900 hover:text-white rounded p-1" href="{{route('professors.edit',$professor->id)}}">edit</a>
-            </div>
+</form>
 
-            
-            
-        @endforeach
-            </div>
-        </div>
-    @endif
+@error('search')
+    <div class="flex justify-center text-red-800 text-lg">Please enter the professors first and last name</div>
+@enderror
+
+@if (!empty($professors))
+    <div class="flex justify-center ">
+    <div >
+
+    @foreach ($professors as $professor)
+        <div class="m-3 border-2 flex shadow-md">
+        <h3 class=" text-2xl p-2 max-w-sm">{{$professor->firstName . " " . $professor->lastName . " - " . $professor->department}}</h3>
+        <form  class="m-2" method="POST" action="{{ route('professors.destroy', $professor->id) }}">
+            @csrf
+            @method('delete')
+            <button class="bg-gray-200 hover:bg-red-900 hover:text-white rounded p-1">Delete</button>
+        </form>     
+        <a class="m-2 bg-gray-200 hover:bg-blue-900 hover:text-white rounded p-1" href="{{route('professors.edit',$professor->id)}}">edit</a>
+        </div>        
+    @endforeach
+
+    </div>
+    </div>
+@endif
 
 </x-header>
