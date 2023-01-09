428d357757229ddd308018153a2adc3facbab8ca
idugan100 <isaacd4444@gmail.com>
2023-01-09 12:21:49 -0500

    styling reviews page

--- resources/views/professors/show.blade.php
+++ resources/views/professors/show.blade.php
@@ -1,8 +1,18 @@
 <x-header>
-    <h1 class="text-xl font-bold underline">{{$professor->firstName . " " . $professor->lastName}}</h1>
-    @foreach ($professor->reviews as $item)
-    <p>{{$item->response}}</p>
-    <hr>
+    <h2 class="bg-yellow-500 text-center font-bold text-white text-xl">{{"Reviews For" . $professor->firstName . " " . $professor->lastName}}</h2>
+    <div class=" grid grid-cols-3 bg-gray-100">
         
-    @endforeach
+        <div class="col-start-2">
+        @foreach ($professor->reviews as $item)
+        <div class="bg-white m-2 p-2 shadow-md">
+            <h3 class="text-lg font-bold underline">{{$item->question}}</h3>
+            <p>{{$item->response}}</p>
+        </div>   
+        @endforeach
+        </div>
+        
+        
+
+    </div>
+    
 </x-header>
