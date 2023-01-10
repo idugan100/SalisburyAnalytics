d0a912d30772b31745166fef6d5be8bec5dab27d
idugan100 <isaacd4444@gmail.com>
2023-01-10 18:33:41 -0500

    reviews index controller and basic layout

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -4,6 +4,7 @@
         <a href="{{route("professors.create")}}" class="bg-red-900 p-1 rounded">New Professor</a> 
         <a href="{{route("reviews.create")}}" class="bg-red-900 p-1 rounded">New Review</a> 
     @endauth
+    <a href="{{route("reviews.index")}}" class="bg-red-900 p-1 rounded">All Reviews</a>
     <a href="{{route("professors.index")}}" class="bg-red-900 p-1 rounded">All Professors</a>
     <a href="{{route("courses.index")}}" class="bg-red-900 p-1 rounded">All Courses</a>
     <a class="bg-red-900 p-1 rounded" href="/">About</a>
