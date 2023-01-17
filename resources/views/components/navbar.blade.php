67a5acaf9d168397cd37bf43fdb3d7aec57a6656
idugan100 <isaacd4444@gmail.com>
2023-01-16 21:26:03 -0500

    added home link to nav bar and fixed auth styling bugs

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -3,6 +3,8 @@
         <a class=" hover:underline px-5 py-2 rounded-lg bold" href="{{route("courses.create")}}">new course</a>
         <a href="{{route("professors.create")}}" class=" hover:underline px-5 py-2 rounded-lg bold ">new professor</a> 
         <a href="{{route("reviews.create")}}" class=" hover:underline px-5 py-2 rounded-lg bold ">new review</a> 
+        <a href="/home" class=" hover:underline px-5 py-2 rounded-lg bold ">home</a> 
+
     @endauth
     <a href="{{route("reviews.index")}}" class=" hover:underline px-5 py-2 rounded-lg bold ">all reviews</a>
     <a href="{{route("professors.index")}}" class="hover:underline px-5 py-2 rounded-lg bold ">all professors</a>
