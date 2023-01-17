ae58649726425fbfa560df5d27e974e4cd611d12
idugan100 <isaacd4444@gmail.com>
2023-01-16 23:17:47 -0500

    added google form to about page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,14 +4,17 @@
             <h2 class="text-2xl font-bold">About This Site</h2>
             <p>This site is a forum for course and professor information that is focused on postive and informative feedback. Many educational review sites 
                 are filled with rants that are unhelpful and often baseless. By partnering directly with universities we can provide the most accurate, extensive
-                and constuctive educational review site.
+                and constuctive educational review site. if you fill out the below form, we'll make sure your review fits with our vision and make it public!
             </p>
-            <p>As or right now, there is a school policy that forbids the public access of any staff evaluation information.
-                If you'd like this site to be fully functional, please contact the SGA, SU senate or a department chair.
-            </p>
+            <hr>
             <p>
                 This site is specifically for Salisbury Univeristy. If you would be interested in this service at your school let us know and we'll get to work!
             </p>
+            <hr>
+            <p>Feel free to contact me at idugan1@salisbury.edu with questions, concerns, and feedback.</p>
+
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd-mBtpKJJ-377ex-86Aa8DpzBnRd6PUoJBcqsfUUJ9hhnvNA/viewform?embedded=true" width="640" height="1468" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            
             <br>
             <p>Feel free to contact me at idugan1@salisbury.edu with questions, concerns, and feedback.</p>
 
