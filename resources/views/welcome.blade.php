cd90d2f8abbafdc124202baf736191c94ffb7cf1
idugan100 <isaacd4444@gmail.com>
2023-01-17 22:28:18 -0500

    about page styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,17 @@
 <x-header>
+    <h2 class="bg-black text-center font-bold text-white text-2xl">about</h2>
+
     <div class="flex justify-center">
-        <div class="max-w-3xl">
-            <h2 class="text-2xl font-bold">About This Site</h2>
-            <p>This site is a forum for course and professor information that is focused on postive and informative feedback. Many educational review sites 
+        <div class="max-w-3xl p-2">
+            <p class="text-lg m-2 ">This site is a forum for course and professor information that is focused on postive and informative feedback. Many educational review sites 
                 are filled with rants that are unhelpful and often baseless. By partnering directly with universities we can provide the most accurate, extensive
                 and constuctive educational review site. if you fill out the below form, we'll make sure your review fits with our vision and make it public!
             </p>
-            <hr>
-            <p>
+            <p class="text-lg m-2">
                 This site is specifically for Salisbury Univeristy. If you would be interested in this service at your school let us know and we'll get to work!
             </p>
-            <hr>
-            <p>Feel free to contact me at idugan1@salisbury.edu with questions, concerns, and feedback.</p>
+            
+            <p class="text-lg m-2">Feel free to contact me at idugan1@salisbury.edu with questions, concerns, and feedback.</p>
 
             <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd-mBtpKJJ-377ex-86Aa8DpzBnRd6PUoJBcqsfUUJ9hhnvNA/viewform?embedded=true" width="640" height="1468" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
             
