7f15279d168b0cf24949ec80354043d8131df1f5
idugan100 <isaacd4444@gmail.com>
2023-02-08 12:05:27 -0500

    added exclamintion mark for group example

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -2,7 +2,7 @@
     <x-subHeader :title="'reviews for '.$course->departmentCode . '-' . $course->courseNumber"/>
     <div class=" grid grid-cols-3 ">
         @if (count($course->reviews)==0)
-            <div class="flex justify-center col-start-2  text-lg">Sorry no reviews found for this course</div>
+            <div class="flex justify-center col-start-2  text-lg">Sorry no reviews found for this course!</div>
         @endif
         <div class="col-start-2">
             @foreach ($course->reviews as $review)
