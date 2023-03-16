3a740124e3b3fb7543254cdad30f3244ca89c4a3
idugan100 <isaacd4444@gmail.com>
2023-03-15 22:21:34 -0400

    admin facing pending review console

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Review;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $reviews=Review::where('approved_flag',False)->latest()->paginate(10);
+        return view('home', compact('reviews'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,6 +20,42 @@
                     <a href="/">To home page</a>
                 </div>
             </div>
+            <div class="card">
+                <div class="card-header">Pending Reviews</div>
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                          <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Professor</th>
+                            <th scope="col">Course</th>
+                            <th scope="col">Review</th>
+                            <th scope="col">Action</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($reviews as $review)
+                            <tr>
+                                <th scope="row">{{$review->id}}</th>
+                                <td>{{$review->professor->firstName . " " . $review->professor->lastName}}</td>
+                                <th scope="row">{{$review->course->departmentCode . "-" . $review->course->courseNumber}}</th>
+                                <td>{{$review->response}}</td>
+                                <td>
+                                    <a class="btn btn-success btn-sm m-2 "href="">Approve</a>
+                                    <a class="btn btn-danger btn-sm m-2"href="">Reject</a>
+
+                                </td>
+                              </tr>
+                            @endforeach
+                            <div>
+                                {{$reviews->links()}}
+
+                            </div>
+                          
+                        </tbody>
+                      </table>
+                </div>
+            </div>
         </div>
     </div>
 </div>
