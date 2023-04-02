927d00ff198d5a6f18f4051a9829633cd8ab7447
idugan100 <isaacd4444@gmail.com>
2023-04-01 23:25:13 -0400

    restyled the professor cards

--- resources/views/components/professorCard.blade.php
+++ resources/views/components/professorCard.blade.php
@@ -1,5 +1,6 @@
-<div class=" contrast-125 p-3 shadow-md shadow-sky-500 bg-white rounded-lg m-2 p-2 ">
-    <h3 class=" text-2xl p-1 bg-zinc-300 px-2 font-bold underline ">{{$professor->firstName . " " . $professor->lastName . " - " . $professor->department}}</h3>
+<div class=" w-64 contrast-125 p-3 shadow-lg shadow-sky-600 bg-white hover:bg-sky-500 rounded-lg m-2 p-5 ">
+    <h3 class=" text-2xl p-1  px-2 font-bold  ">{{$professor->firstName . " " . $professor->lastName }}</h3>
+    <h4 class="text-lg  p-1  px-2  text-gray-500 font-bold">{{$professor->department}}</h4>
     <div class="flex rounded-md">
         @auth
             <form  class="" method="POST" action="{{ route('professors.destroy', $professor->id) }}">
@@ -9,6 +10,6 @@
             </form>     
             <a class="m-1  border-2 border-sky-500 hover:text-white hover:bg-sky-500 rounded px-1 " href="{{route('professors.edit',$professor->id)}}">edit</a>
         @endauth
-        <a class="m-1  border-2 border-sky-500 hover:text-white hover:bg-sky-500 rounded px-1 " href="{{route('professors.show',$professor->id)}}">reviews</a>
+        <a class="m-1  border-4 border-sky-500 hover:text-white bg-sky-500 rounded font-bold px-1" href="{{route('professors.show',$professor->id)}}">reviews</a>
     </div>
 </div>   
