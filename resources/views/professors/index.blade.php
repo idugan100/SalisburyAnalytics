5f5915648508003c738651e4faf1ae36b4b3a7d5
idugan100 <isaacd4444@gmail.com>
2023-04-01 23:25:13 -0400

    restyled the professor cards

--- resources/views/professors/index.blade.php
+++ resources/views/professors/index.blade.php
@@ -1,7 +1,7 @@
 <x-header>
 
 <x-subHeader title="all professors"/>
-<form class="mt-3 flex justify-center" action="{{route('professors.index')}}">
+<form class="my-5 flex justify-center" action="{{route('professors.index')}}">
     @method('get')
     <input placeholder ="John Smith" class="border-3 p-2  " name ="search" type="text">
     <button class="p-2 hover:underline rounded-md  font-bold" type="submit">Search</button>
@@ -15,10 +15,12 @@
   <p class="flex justify-center text-lg" >We couldn't find that professor. Please double check your spelling.</p>
   
 @elseif (!empty($professors))
-    <div class="grid lg:grid-cols-6 grid-cols-3">
-    @foreach ($professors as $professor)
-        <x-professorCard :professor="$professor"></x-professorCard>
-    @endforeach
+    <div class="flex justify-center">
+        <div class=" grid lg:grid-cols-4 gap-4 grid-cols-3">
+            @foreach ($professors as $professor)
+                <x-professorCard :professor="$professor"></x-professorCard>
+            @endforeach
+            </div>
     </div>
 @endif
 
