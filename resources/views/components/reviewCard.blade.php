687a48b77c0f6f24ddc08a699ad9604ddd7b7dae
idugan100 <isaacd4444@gmail.com>
2023-04-05 20:33:08 -0400

    changed font sizes on the create review page to match the font szes on the other cards

--- resources/views/components/reviewCard.blade.php
+++ resources/views/components/reviewCard.blade.php
@@ -1,21 +1,3 @@
-{{-- 
- <div class="bg-white  rounded-lg m-6 contrast-125 p-3 shadow-lg shadow-sky-500">
-    @if (isset($review->question))
-        <h3 class=" text-lg font-bold bg-zinc-300 p-1 underline">{{$review->question}}</h3>
-    @endif
-    <p class="m-2">{{$review->response}}</p>
-    <hr class=" border-black border">
-    <div class="bg-gray-200 rounded p-1 my-2">{{$review->course->departmentCode . "-" . $review->course->courseNumber . " | " . $review->professor->firstName . " ". $review->professor->lastName}}</div>
-    @auth
-    <form  class="m-2" method="POST" action="{{ route('reviews.destroy', $review->id) }}">
-        @csrf
-        @method('DELETE')
-        <button class="m-1  border-2 border-sky-500 hover:text-white hover:bg-sky-500 rounded px-1">Delete</button>
-    </form>
-    @endauth
-</div>    --}}
-
-
 <div class="  contrast-125 group p-3 shadow-lg  shadow-black  bg-white hover:bg-sky-500 rounded-lg m-2 p-5 ">
     <h3 class=" text-2xl py-1 px-2 font-bold ">{{$review->course->departmentCode . "-" . $review->course->courseNumber}}</h3>
     <hr class="border-1 border-sky-500 ">
