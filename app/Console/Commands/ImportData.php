d1b52abaf9712c78467e53075769ac63c822f540
isaac dugan <isaacd4444@gmail.com>
2023-06-11 16:38:51 -0400

    added header function to import:data function and logic to process each record and print out it's contents in a table before calling the various insert functions

--- app/Console/Commands/ImportData.php
+++ app/Console/Commands/ImportData.php
@@ -27,6 +27,9 @@ class ImportData extends Command
      *
      * @return int
      */
+
+     //TOdo implemnt transactions -https://laravel.com/docs/10.x/database#database-transactions
+     //TODO implement try catch
     public function handle()
     {
         $file_path = $this->ask('Enter the filepath you are going to import: ');
@@ -34,16 +37,58 @@ class ImportData extends Command
         $reader = ReaderEntityFactory::createReaderFromFile($file_path);
 
         $reader->open($file_path);
+        $headers=$this->getHeaders($reader);
+        $confirm_headers = $this->confirm('Are the following headers correct: ' . implode("," ,$headers));
+        //add exit if they are not confirmed
         $row_counter=0;
         foreach ($reader->getSheetIterator() as $sheet) {
-            foreach ($sheet->getRowIterator() as $row) {
-                // do stuff with the row
-                $cells = $row->getCells();
+            foreach ($sheet->getRowIterator() as $row_number => $row) {
+                if($row_number>1){ //not done for header row
+                    $data=$this->rowToArray($row);
+                    $this->table($headers,[$data]);
+                    $course_ID=$this->insertCourse($data);
+                    $professor_ID=$this->insertProfessor($data);
+                    $this->createGradeLineIterm($course_ID,$professor_ID);
+                }
                 $row_counter++;
             }
         }
 
-        echo "import completed ". $row_counter . " records processed";
+        $this->info( "import completed ". "{{set up counter here}}" . " records processed");
         return Command::SUCCESS;
+    }
+
+    private function getHeaders($reader){
+        
+        foreach ($reader->getSheetIterator() as $sheet) {
+            foreach ($sheet->getRowIterator() as $row) {
+                return $this->rowToArray($row);
+            }
+        }
+    }
+
+    private function rowToArray($row){
+        $data_array=[];
+        foreach($row->getCells() as $cell){
+            $data_array[]=$cell->getValue();
+        }
+        return $data_array;
+    }
+
+    private function insertCourse($data){
+        //implement insertion
+        echo("inserting course \n");
+        return 2;
+    }
+
+    private function insertProfessor($data){
+        //implment insertion
+        echo("inserting professor \n");
+        return 5;
+    }
+
+    private function createGradeLineIterm($course_ID, $professor_ID){
+        //implement insertion
+        echo("inserting grade line item \n");
     }
 }
