328a6b7b4822a6cf6c366a41b825a953b55b0e9d
isaac dugan <isaacd4444@gmail.com>
2023-06-12 20:16:51 -0400

    wrote add insertCourse function that checks if the course exists and if it doesn't promts for the information needs to insert the course and then returns the course id

--- app/Console/Commands/ImportData.php
+++ app/Console/Commands/ImportData.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Command;
 use Exception;
 use Box\Spout\Reader\Common\Creator\ReaderEntityFactory;
 use Illuminate\Support\Facades\DB;
+use App\Models\Course;
 
 
 class ImportData extends Command
@@ -37,9 +38,9 @@ class ImportData extends Command
             $file_path = $this->ask('Enter the filepath you are going to import: ');
             $reader = ReaderEntityFactory::createReaderFromFile($file_path);
             $reader->open($file_path);
-            
+
             $headers=$this->getHeaders($reader);
-            $confirm_headers = $this->confirm('Are the following headers correct: ' . implode("," ,$headers));
+            $confirm_headers = $this->confirm('Are the following headers correct: ' . implode(", " ,$headers));
             // TODO add exit if they are not confirmed
 
             $row_counter=0;
@@ -50,7 +51,7 @@ class ImportData extends Command
                         $this->table($headers,[$data]);
                         $course_ID=$this->insertCourse($data);
                         $professor_ID=$this->insertProfessor($data);
-                        $this->createGradeLineIterm($course_ID,$professor_ID);
+                        $this->createGradeLineItem($course_ID,$professor_ID);
                     }
                     $row_counter++;
                 }
@@ -86,18 +87,34 @@ class ImportData extends Command
     }
 
     private function insertCourse($data){
-        //implement insertion
-        echo("inserting course \n");
-        return 2;
+        $course=DB::table('courses')->where("courseNumber",$data[2])->where("departmentCode",$data[1])->first();
+        if($course){
+            echo($data[1] . "-" . $data[2] . " found\n");
+            return $course->id;
+        }
+        else{
+            $course_title = $this->ask("What is the name of " . $data[1] . "-" . $data[2] . "?");
+            $course_desciption = $this->ask("What is the course description of " . $data[1] . "-" . $data[2] . "?");
+            $new_course = new Course;
+            $new_course->courseTitle = $course_title;
+            $new_course->description = $course_desciption;
+            $new_course->departmentCode = $data[1];
+            $new_course->courseNumber = $data[2];
+            $new_course->save();
+
+            echo("inserting course " . $data[1] . "-" . $data[2] . "\n");
+            return $new_course->id;
+        }
+        
     }
 
     private function insertProfessor($data){
-        //implment insertion
+        //implement insertion
         echo("inserting professor \n");
         return 5;
     }
 
-    private function createGradeLineIterm($course_ID, $professor_ID){
+    private function createGradeLineItem($course_ID, $professor_ID){
         //implement insertion
         echo("inserting grade line item \n");
     }
