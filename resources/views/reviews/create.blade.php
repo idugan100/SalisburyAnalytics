d37f11a2b305584d2997e968dbdce0eee9a4e06c
isaac dugan <isaacd4444@gmail.com>
2023-06-21 23:20:38 -0400

    switched professor name in review from to lastname, firstname and switch migration to make response a medium text field

--- resources/views/reviews/create.blade.php
+++ resources/views/reviews/create.blade.php
@@ -30,7 +30,7 @@
                         <select id="category" name="professorID" class="bg-gray-200 border border-gray-200 text-gray-900 text-sm rounded-lg focus:ring-sky-500 focus:border-sky-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                             <option value="" selected>Select professor</option>
                             @foreach ($professorList as $professor)
-                                <option value="{{$professor->id}}">{{$professor->firstName . " " . $professor->lastName}}</option>
+                                <option value="{{$professor->id}}">{{$professor->lastName . ", " . $professor->firstName}}</option>
                             @endforeach
                         </select>
                     </div>
