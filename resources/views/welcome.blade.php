ca764dedd6ef93938d233941b3e646146c6b7902
isaac dugan <isaacd4444@gmail.com>
2023-06-24 23:18:47 -0400

    changed welcome page content and added the frequenty professor/course to modal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,8 @@
 <x-subHeader title="about"/>
     <div class="flex justify-center">
         <div class="max-w-3xl bg-white mt-16  rounded-lg shadow-lg shadow-black p-2 divide-y-4 divide-sky-500 divide-dotted">
-            <p class="text-2xl m-4 p-4">This site is a forum for course and professor information that is focused on constructive and quantitative feedback. Many educational review sites 
-                are filled with unhelpful rants. Through the Public Information Act, we can provide detailed grade and course information about Salisbury University. 
-                We also anonmyously host appropriate student reviews!
+            <p class="text-2xl m-4 p-4">Through the Public Information Act, we can provide detailed grade and course information about Salisbury University. 
+                We also anonmyously host appropriate student reviews! Our goal is to provide students with the tools to make informed course selection decisions.
             </p>
             <p class="text-2xl m-4 p-4">
                 This site is specifically for Salisbury Univeristy. If you would be interested in this service at your school, let me know and we'll get to work!
