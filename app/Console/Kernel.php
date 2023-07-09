f4dfb09612f73e308b714d83b3de32ac131c0dd5
isaac dugan <isaacd4444@gmail.com>
2023-07-08 23:49:31 -0400

    added migration for usage log table and scheduled command to make the daily record

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
+use App\Models\UsageLog;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
@@ -16,6 +17,12 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            $usage_log= new UsageLog();
+            $usage_log->save();
+ 
+         })->daily();
+ 
     }
 
     /**
@@ -26,7 +33,7 @@ class Kernel extends ConsoleKernel
     protected function commands()
     {
         $this->load(__DIR__.'/Commands');
-
+       
         require base_path('routes/console.php');
     }
 }
