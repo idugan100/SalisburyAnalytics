2e3fd36a4b839c992b3f3ca2fcb9497f37cddc1a
isaac dugan <isaacd4444@gmail.com>
2023-07-11 09:59:05 -0400

    changed admin nav items to be undelined with yellow instead of blue

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,8 +1,8 @@
 <nav class="p-3   text-xl font-sans font-bold px-5 py-2 rounded-lg">
     @auth
-        <a class=" decoration-sky-500 decoration-4 hover:underline px-5 py-2 rounded-lg decoration-blue-400 bold" href="{{route("courses.create")}}">new course</a>
-        <a href="{{route("professors.create")}}" class="decoration-sky-500 decoration-4 hover:underline px-5 py-2 rounded-lg bold ">new professor</a> 
-        <a href="{{route("reviews.processing")}}" class=" decoration-sky-500 decoration-4 hover:underline px-5 py-2 rounded-lg bold ">home</a> 
+        <a class=" decoration-yellow-500 decoration-4 hover:underline px-5 py-2 rounded-lg decoration-4 bold" href="{{route("courses.create")}}">new course</a>
+        <a href="{{route("professors.create")}}" class="decoration-yellow-500 decoration-4 hover:underline px-5 py-2 rounded-lg bold ">new professor</a> 
+        <a href="{{route("reviews.processing")}}" class=" decoration-yellow-500 decoration-4 hover:underline px-5 py-2 rounded-lg bold ">home</a> 
 
     @endauth
     <a href="{{route("reviews.index")}}" class=" decoration-yellow-500 decoration-4 hover:underline px-5 py-2 rounded-lg bold ">all reviews</a>
