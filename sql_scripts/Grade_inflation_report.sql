cd92b112bbbb319bd12a36012475c80f86a7a188
isaac dugan <isaacd4444@gmail.com>
2023-07-12 01:16:04 -0400

    grade inflation report sql and controller

--- sql_scripts/Grade_inflation_report.sql
+++ sql_scripts/Grade_inflation_report.sql
@@ -1,5 +1,6 @@
-Select sum(T.GPA)/sum(T.quantity) as 'Course GPA' from
-(Select grade, quantity,
+Select sum(T.GPA)/sum(T.quantity) as 'GPA', semester, `year` from
+(
+Select  quantity, `year`, semester,
         CASE 
 		WHEN grade='A' THEN 4 
         WHEN grade="B" THEN 3 
@@ -10,4 +11,8 @@ Select sum(T.GPA)/sum(T.quantity) as 'Course GPA' from
         END * quantity as 'GPA'
 from courses_x_professors_with_grades
 join courses on course_ID=courses.id
-where  grade in ("A","B","C","D","F","W") )as `T`;
+where  grade in ("A","B","C","D","F","W"))as `T`
+
+group by
+year, semester
+order by year, semester DESC;
