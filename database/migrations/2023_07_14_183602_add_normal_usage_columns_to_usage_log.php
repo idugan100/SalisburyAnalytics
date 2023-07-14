c6023a94b2cee3fc33631fc8e3c7cca9dd4c0490
isaac dugan <isaacd4444@gmail.com>
2023-07-14 14:37:38 -0400

    migrations for bot sensitive tracking

---
+++ database/migrations/2023_07_14_183602_add_normal_usage_columns_to_usage_log.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('usage_log', function (Blueprint $table) {
+            $table->integer("about_views")->default(0);
+            $table->integer("course_views")->default(0);
+            $table->integer("professor_views")->default(0);
+            $table->integer("review_views")->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('usage_log', function (Blueprint $table) {
+            $table->dropColumn("about_views");
+            $table->dropColumn("course_views");
+            $table->dropColumn("professor_views");
+            $table->dropColumn("review_views");
+        });
+    }
+};
