9810b98593749b98b9c237f348cbbd33965edee5
isaac dugan <isaacd4444@gmail.com>
2023-07-16 00:05:04 -0400

    user details migration

---
+++ database/migrations/2023_07_16_040107_create_user_details.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_details', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->intger("usage_log_id");
+            $table->string("user_agent");
+            $table->string("ip_adress");
+            $table->sting("page_visited");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_details');
+    }
+};
