45aced3a1428640e07ac12411881a3b5e61f7fc4
isaac dugan <isaacd4444@gmail.com>
2023-07-20 23:18:06 -0400

    and sql scripts for enrollment and gpa tracking reports

---
+++ sql_scripts/Enrollment_Tracking.sql
@@ -0,0 +1,17 @@
+Select sum(quantity) as 'Enrollment', semester, `year` from
+                (Select  quantity, `year`, semester,
+                        CASE 
+                        WHEN grade='A' THEN 4 
+                        WHEN grade='B' THEN 3 
+                        WHEN grade='C' THEN 2
+                        WHEN grade='D' THEN 1
+                        WHEN grade='F' THEN 0
+                        else 0
+                        END * quantity as 'GPA'
+                from courses_x_professors_with_grades
+                join courses on course_ID=courses.id
+                where  departmentCode like '%ENVR%')as `T`
+                group by
+                year, semester
+                order by 
+                year, semester DESC;
