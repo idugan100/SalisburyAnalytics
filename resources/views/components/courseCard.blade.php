3cfbb791d9ca862855560fbf0f271c88b9ecb88d
isaac dugan <isaacd4444@gmail.com>
2023-07-24 23:49:11 -0400

    changing modal from course labeled to review labled

--- resources/views/components/courseCard.blade.php
+++ resources/views/components/courseCard.blade.php
@@ -19,14 +19,14 @@
             </form>     
             <a class="m-2  border-4 border-yellow-500 hover:text-gray-300 bg-yellow-500 rounded font-bold px-1" href="{{route('courses.edit',$course->id)}}">edit</a>
         @endauth
-        <button data-modal-target="{{"Course-Modal-".$course->id}}" data-modal-toggle="{{"Course-Modal-".$course->id}}" class="m-2  border-4 border-yellow-500 hover:text-gray-300 bg-yellow-500 rounded font-bold px-1" type="button">reviews</button>
+        <button data-modal-target="{{"Review-Modal-".$course->id}}" data-modal-toggle="{{"Review-Modal-".$course->id}}" class="m-2  border-4 border-yellow-500 hover:text-gray-300 bg-yellow-500 rounded font-bold px-1" type="button">reviews</button>
         <a class="m-2  border-4 border-yellow-500 hover:text-gray-300 bg-yellow-500 rounded font-bold px-1" href="{{route('courses.show',$course->id)}}">grades</a>
     </div>
 </div>  
 
 
 <!-- Modal content -->
-<div id="{{"Course-Modal-".$course->id}}" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+<div id="{{"Review-Modal-".$course->id}}" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative w-full max-w-2xl max-h-full">
         
         <div class="relative bg-white rounded-lg shadow ">
@@ -35,7 +35,7 @@
                 <h3 class="text-xl font-semibold text-gray-900 ">
                     {{"Reviews for " . $course->departmentCode ."-" . $course->courseNumber }} 
                 </h3>
-                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center " data-modal-hide={{"Course-Modal-".$course->id}}>
+                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center " data-modal-hide={{"Review-Modal-".$course->id}}>
                     <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                     <span class="sr-only">Close modal</span>
                 </button>
@@ -47,7 +47,7 @@
                 </div>
 
                     @if (count($course->reviews)==0)
-                    <div class="flex justify-center col-start-2  text-lg">Sorry no reviews found for this professor</div>
+                    <div class="flex justify-center col-start-2  text-lg">Sorry, no reviews found for this course :(</div>
                     @endif
                     <div class="col-start-2 col-span-2 mt-4">
                         @foreach ($course->reviews as $review)
