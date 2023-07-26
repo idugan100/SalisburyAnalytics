1b1941ef430655936716518d93c8ebeeae07db22
isaac dugan <isaacd4444@gmail.com>
2023-07-25 23:17:41 -0400

    updated migration to allow rmp_link to be nullable

--- database/migrations/2023_06_27_221121_add_rmp_link_to_professors.php
+++ database/migrations/2023_06_27_221121_add_rmp_link_to_professors.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('professors', function (Blueprint $table) {
-            $table->string("rmp_link");
+            $table->string("rmp_link")->nullable();
         });
     }
 
