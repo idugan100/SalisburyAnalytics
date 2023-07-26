51c2cd3f3c0998f032fc325c53c02fe01af41ac1
isaac dugan <isaacd4444@gmail.com>
2023-07-25 22:59:54 -0400

    removing dark styles from nav

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="bg-gray-200 border-gray-200 dark:bg-gray-900">
+<nav class="bg-gray-200 border-gray-200 ">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       
       <button data-collapse-toggle="navbar-solid-bg" type="button" class=" no-highlights inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 " aria-controls="navbar-default" aria-expanded="false">
@@ -8,7 +8,7 @@
           </svg>
       </button>
       <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
-        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-yellow-500 rounded-lg bg-gray-200 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-gray-200 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-yellow-500 rounded-lg bg-gray-200 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-gray-200 ">
            
             @auth
                 <li>
