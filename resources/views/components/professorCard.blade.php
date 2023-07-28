d40d5c8dfbf8e1b9d7b7df9b37268371aa0a821d
isaac dugan <isaacd4444@gmail.com>
2023-07-27 22:43:13 -0400

    changing no reviews froud error messgae from course to professor

--- resources/views/components/professorCard.blade.php
+++ resources/views/components/professorCard.blade.php
@@ -56,7 +56,7 @@
                 </div>
 
                     @if (count($professor->reviews)==0)
-                    <div class="flex justify-center col-start-2  text-lg">Sorry, no reviews found for this course :(</div>
+                    <div class="flex justify-center col-start-2  text-lg">Sorry, no reviews found for this professor :(</div>
                     @endif
                     <div class="col-start-2 col-span-2 mt-4">
                         @foreach ($professor->reviews as $review)
