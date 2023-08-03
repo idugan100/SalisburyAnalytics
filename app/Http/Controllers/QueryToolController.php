0ff275d82f9c9467cd6f1bccfcd0d127c8b331c6
isaac dugan <isaacd4444@gmail.com>
2023-08-03 13:56:04 -0400

    got query tool controller tenatively working, still need to do data return and view

--- app/Http/Controllers/QueryToolController.php
+++ app/Http/Controllers/QueryToolController.php
@@ -11,7 +11,8 @@ class QueryToolController extends Controller
     public function index(Request $request){
         $departments=Course::select("departmentCode")->orderBy("departmentCode")->distinct()->get();
         if(isset($request->quantity)){
-            $query=$this->setEntity($request->entity);
+            $results=$this->getData($request);
+            dd($results);
         }
 
         return view("queryTool.index",
@@ -20,15 +21,41 @@ class QueryToolController extends Controller
             ]);
     }
 
-    private function setEntity($entity){
-        if($entity=="courses"){
-            return DB::table("courses")->select("courseTitle","courseNumber","courses.id","departmentCode","statistic");
+    private function getData($request){
+        if($request->entity=="courses"){
+            if(isset($request->department_filter)){
+                // dd($request->department_filter);
+                return DB::table("courses")->select("courseTitle", "courseNumber", "courses.id", "departmentCode",$request->statistic)
+                        ->where("courses.departmentCode",$request->department_filter)
+                        ->orderBy($request->statistic, $request->ordering)
+                        ->limit($request->quantity)
+                        ->get();
+            }
+            else{
+                return DB::table("courses")->select("courseTitle", "courseNumber", "courses.id", "departmentCode",$request->statistic)
+                        ->orderBy("courses.".$request->statistic, $request->ordering)
+                        ->limit($request->quantity)
+                        ->get();
+            }
         }
-        elseif($entity=="professors"){
-           return DB::table("professors")->select("firstName","lastName","professors.id","statistic");
-        }
-        else{
-            return DB::table("courses")->select("departmentCode","statistic");
+        elseif($request->entity=="professors"){
+            if(isset($request->department_filter)){
+                return DB::table("professors")->selectRaw("professors.*")
+                        ->join("courses_x_professors_with_grades","professor_ID","professors.id")
+                        ->join("courses","courses.id","course_ID")
+                        ->where("departmentCode",$request->department_filter)
+                        ->groupBy("professor_id",$request->statistic)
+                        ->orderBy($request->statistic, $request->ordering)
+                        ->limit($request->quantity)
+                        ->get();
+            }
+            else{
+                return DB::table("professors")->select("firstName", "lastName", "professors.id" ,$request->statistic)
+                        ->orderBy($request->statistic, $request->ordering)
+                        ->limit($request->quantity)
+                        ->get();
+            }
         }
     }
+    
 }
