ce09870a399316ef69faeeab2a32a5b06947823e
isaac dugan <isaacd4444@gmail.com>
2023-08-17 01:09:11 -0400

    migrated professor search from livewire to htmx

--- resources/views/professors/index.blade.php
+++ resources/views/professors/index.blade.php
@@ -1,11 +1,32 @@
 <x-header>
 
 <x-subHeader title="search professors"/>
-<livewire:search-professors :selected_department="$result_department" :selected_professor='$result_professor'/>
+
+<form class=" m-3 flex justify-center" action="{{route('professors.index')}}">
+    @method('get')
+    <select class="border-2 p-2 border-black " name="department" hx-get="/professor_options_by_department" hx-target="#professorSelect">
+        <option  value="{{null}}" >select department</option>
+       @foreach ($departments as $department)
+           <option value="{{$department["departmentCode"]}}" >{{$department["departmentCode"]}}</option>
+       @endforeach
+    </select>
+    <select class="border-2 p-2 border-black w-1/3" name="professor_id" id="professorSelect">
+        <option value="" >select professor</option>
+    </select>
+    <button class="p-2 border-2 border-black hover:bg-black hover:text-white" type="submit">Search</button>
+</form>
 
 @error('search')
     <div class="flex justify-center  text-lg">Please enter the professors first and last name</div>
 @enderror
+
+@isset($message)
+    <div class="flex justify-center">
+        <div class="font-bold m-2">
+            {{$message}}
+        </div>
+    </div>
+@endisset
 
 @if(count($professors)==0)
   <p class="flex justify-center flex-wrap text-lg" >We couldn't find that professor. Please double check your spelling.</p>
