f28a8054a1f92f015ef156315f06ddcc8e161ebc
Isaac Dugan <isaacdugan@Isaacs-MacBook-Pro.local>
2023-08-23 23:50:44 -0400

    removed dark mode from add review card

--- resources/views/reviews/create-options.blade.php
+++ resources/views/reviews/create-options.blade.php
@@ -1,6 +1,6 @@
 <div class="p-2">
-    <label for="courseSelection" class="block mb-2 font-bold text-md text-black dark:text-white">Course</label>
-    <select   id="courseSelection" name="courseID" class="bg-gray-200 border border-gray-200 text-black text-sm rounded-lg focus:ring-red-700 focus:border-red-700 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
+    <label for="courseSelection" class="block mb-2 font-bold text-md text-black">Course</label>
+    <select   id="courseSelection" name="courseID" class="bg-gray-200 border border-gray-200 text-black text-sm rounded-lg focus:ring-red-700 focus:border-red-700 block w-full p-2.5" required>
         <option value="">Select course</option>
         @foreach ($courseList as $course)
             <option value="{{$course->id}}">{{$course->courseNumber . " - " . $course->courseTitle}}</option>
@@ -8,8 +8,8 @@
     </select>
 </div>
 <div class="p-2">
-    <label for="professorSelection" class="block mb-2 text-md font-bold text-black dark:text-white">Professor</label>
-    <select required id="professorSelection" name="professorID" class="bg-gray-200 border border-gray-200 text-gray-900 text-sm rounded-lg focus:ring-red-700 focus:border-red-700 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+    <label for="professorSelection" class="block mb-2 text-md font-bold text-black ">Professor</label>
+    <select required id="professorSelection" name="professorID" class="bg-gray-200 border border-gray-200 text-gray-900 text-sm rounded-lg focus:ring-red-700 focus:border-red-700 block w-full p-2.5">
         <option value="" selected>Select professor</option>
         @foreach ($professorList as $professor)
             <option value="{{$professor->id}}">{{$professor->lastName . ", " . $professor->firstName}}</option>
