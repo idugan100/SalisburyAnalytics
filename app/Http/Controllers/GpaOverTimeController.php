09b29a051643c637971ac131e0bb909c7549837a
Isaac Dugan <isaacdugan@Isaacs-MacBook-Pro.local>
2023-09-07 20:24:45 -0400

    security patch

--- app/Http/Controllers/GpaOverTimeController.php
+++ app/Http/Controllers/GpaOverTimeController.php
@@ -30,13 +30,13 @@ class GpaOverTimeController extends Controller
                 from courses_x_professors_with_grades
                 join courses on course_ID=courses.id
                 where  grade in ('A','B','C','D','F')
-                and departmentCode like '%".$selected_department."%')as `T`
+                and departmentCode like ? )as `T`
                 group by
                 year, semester
                 order by 
                 year, semester DESC;";
 
-        $gpa_by_semester=DB::select($query);
+        $gpa_by_semester=DB::select($query,[($selected_department ?? "%")]);
             
       
         $gpa_chart=$chart->build($gpa_by_semester, $selected_department);
