3f9c584d5990be1133f9b6a62f493c962c9208e8
idugan100 <isaacd4444@gmail.com>
2023-12-25 00:17:44 -0500

    refactoring and unit testing for isbot functionality

--- app/services/IsBot.php
+++ app/services/IsBot.php
@@ -2,18 +2,15 @@
 
 namespace App\services;
 
-use Illuminate\Http\Request;
-
-
 class IsBot
 {
-    public static function check(Request $request)
+    public static function check(string $ip, string $user_agent)
     {
-        $user_agent = preg_match('/.*bot.*|.*python-requests.*|.*http.*|.*node-fetch.*|.*postman.*|.*curl.*|.*Bytespider.*/', $request->userAgent());
-        $ip = preg_match(
-            '/54.198.193.239|23.20.205.139|54.198.181.10|54.237.245.103|52.90.196.158|3.84.129.243|52.90.196.158|54.173.157.97|44.203.91.18|3.81.172.31|54.183.164.142|44.200.174.154|54.243.7.211|34.204.78.11|54.159.21.79/'
-            ,$request->ip());
+        $user_agent_result = preg_match('/.*bot.*|.*python-requests.*|.*http.*|.*node-fetch.*|.*postman.*|.*curl.*|.*Bytespider.*/', $user_agent);
+        $ip_result = preg_match(
+            '/54.198.193.239|23.20.205.139|54.198.181.10|54.237.245.103|52.90.196.158|3.84.129.243|52.90.196.158|54.173.157.97|44.203.91.18|3.81.172.31|54.183.164.142|44.200.174.154|54.243.7.211|34.204.78.11|54.159.21.79/', $ip
+        );
 
-        return ($ip || $user_agent);
+        return $user_agent_result || $ip_result;
     }
 }
