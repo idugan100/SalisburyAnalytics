9155e5729dd7b788f85fa43baa34681d7b01feba
idugan100 <isaacd4444@gmail.com>
2024-01-12 21:18:04 -0500

    adding course times page test

--- tests/Feature/CourseTimesTest.php
+++ tests/Feature/CourseTimesTest.php
@@ -2,42 +2,39 @@
 
 namespace Tests\Feature;
 
+use Tests\TestCase;
 use App\Models\Course;
 use App\Models\UsageLog;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
 
-class CourseShowTest extends TestCase
+class CourseTimesTest extends TestCase
 {
     use RefreshDatabase;
-
     /**
      * A basic feature test example.
-     *
-     * @return void
      */
-    public function test_show_course_view()
+    public function test_course_times_view(): void
     {
         UsageLog::factory()->create();
 
         Course::factory()->create();
 
-        $response = $this->get(route('courses.show', 1));
+        $response = $this->get(route('courses.times', 3));
 
         $response->assertStatus(200);
     }
 
-    public function test_show_course_usage_tracking()
+    public function test_course_times_usage_tracking(): void
     {
         UsageLog::factory()->create();
 
         Course::factory()->create();
 
-        $this->get(route('courses.show', 2));
+        $this->get(route('courses.times', 4));
 
         $this->assertDatabaseHas('usage_log', [
             'created_at' => now()->toDateTimeString(),
             'course_views' => 1,
-        ]);  
+        ]); 
     }
 }
