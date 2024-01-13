b9d48643574e5f3dcfcb37dc5c06ea900bce0e8a
idugan100 <isaacd4444@gmail.com>
2024-01-12 20:30:44 -0500

    updating premium and checkout page to reflect new premium features

--- resources/views/billing/premium.blade.php
+++ resources/views/billing/premium.blade.php
@@ -5,7 +5,7 @@
             <h3 class="font-bold m-2">you clicked on a premium feature! for three dollars each month you can have access to the below functionality:</h3>
            </div>
     <ul class="md:flex justify-center flex-wrap">
-        <li class="m-2 border-black h-72 border-2 w-80 p-2 flex flex-col bg-white rounded ">
+        {{-- <li class="m-2 border-black h-72 border-2 w-80 p-2 flex flex-col bg-white rounded ">
             <img src="{{ URL::asset('gpa_chart_image.png') }}" class="h-44 w-96" alt="">
             <div>
                 grade point average and enrollment tracking charts that can be filtered by department to show trends over time
@@ -14,9 +14,9 @@
         <li class="m-2 border-black border-2 h-72 w-80 p-2  flex flex-col bg-white rounded"> 
             <img src="{{ URL::asset('grade_distribution_image.png') }}" alt="" class="h-44 border-2 border-gray-300 w-96">
             <div>grade distributions by course, semester, and professor that gives detailed insight about future classes and lets you see how you stacked up in previous classes</div>
-        </li>
-        <li class="m-2 border-black border-2 h-72 w-80 p-2  flex flex-col bg-white rounded">
-            <img src="{{ URL::asset('query_tool_image.png') }}" alt="" class="h-44  w-80">
+        </li> --}}
+        <li class="m-2 border-black border-2 h-120 w-100 p-2  flex flex-col bg-white rounded">
+            <img src="{{ URL::asset('query_tool_image.png') }}" alt="" class="h-80  w-120">
             <div>query tool that allows you to create custom reports about professors, courses, departments, enrollment, gpa, withdraw rates and more</div>
         </li>
     </ul>
