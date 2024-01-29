0a866aac6fb145facdac78320b50b25baa207872
idugan100 <isaacd4444@gmail.com>
2024-01-29 01:03:34 -0500

    adding coming soon to gullgpt page until iget ssl working

--- resources/views/gullGPT.blade.php
+++ resources/views/gullGPT.blade.php
@@ -1,6 +1,8 @@
 <x-header>
  <x-subHeader title="gullGPT"/>
+
     <div class="p-6 space-y-6 flex justify-center ">
+        <h3 class="font-bold text-xl">coming soon ...</h3>
         <iframe src="http://gullgpt.study" height="700" width="1250" frameborder="0"></iframe>
     </div>
 </x-header>
