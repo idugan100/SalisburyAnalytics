cb04b997d64751683be78097d0f73337a6d5d332
idugan100 <isaacd4444@gmail.com>
2024-02-06 01:45:13 -0500

    adding gullGPT page view and usage tracking tests

--- tests/Feature/GullGptViewTest.php
+++ tests/Feature/GullGptViewTest.php
@@ -2,10 +2,9 @@
 
 namespace Tests\Feature;
 
-use Tests\TestCase;
 use App\Models\UsageLog;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class GullGptViewTest extends TestCase
 {
