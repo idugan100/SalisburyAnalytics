3e4970f12be3f78031bf2f19b59353c582605889
idugan100 <isaacd4444@gmail.com>
2024-02-19 16:55:14 -0500

    migration and model

---
+++ app/Models/FinancialOutcomeInfo.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FinancialOutcomeInfo extends Model
+{
+    use HasFactory;
+    protected $table = 'financial_outcomes';
+
+}
---
+++ database/migrations/2024_02_19_211846_create_financial_outcomes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('financial_outcomes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("program_name");
+            $table->string("credential_name");
+            $table->integer("median_income_year_1");
+            $table->integer("median_income_year_4");
+            $table->float("unemployment_pct_year_1");
+            $table->float("unemployment_pct_year_4");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('financial_outcomes');
+    }
+};
