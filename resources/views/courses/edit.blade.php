e1d84517e5d856865d65ed1e9bfcde048d9dcf25
idugan100 <isaacd4444@gmail.com>
2024-03-11 02:27:43 -0400

    fixing professor edit form

--- resources/views/courses/edit.blade.php
+++ resources/views/courses/edit.blade.php
@@ -18,28 +18,13 @@
         <label class="mr-2">Course Number</label>
         <input class="border-2 border-black" name="courseNumber" type="text" value="{{$course->courseNumber}}" required>
     </div>
-    <label>Course Description</label>
-
     <div class="m-4">
-        <textarea class="border-2 border-black" name="description" cols="30" rows="10" required>{{$course->description}}</textarea>
+        <label>Course Description</label>
     </div> 
     <div class="m-4">
-        <label class="mr-2">Syllabus Link</label>
-        <input class="border-2 border-black" name="syllabusLink" type="text" value="{{$course->syllabusLink}}" >
+        <textarea class="border-2 border-black" name="description" cols="30" rows="10" required>{{$course->description}}</textarea>
     </div>
-    <div class="m-4">
-        <label class="mr-2">Lab Hours Per Week</label>
-        <input class="border-2 border-black" name="creditsLab" type="number" value="{{$course->creditsLab}}" >
-    </div>
-    <div class="m-4">
-        <label class="mr-2">Lecture Hours Per Week</label>
-        <input class="border-2 border-black" name="creditsLecture" type="number" value="{{$course->creditsLecture}}" >
-    </div>
-    <div class="m-4">
-        <label class="mr-2">Credit Hours</label>
-        <input class="border-2 border-black" name="creditsTotal" type="number" value="{{$course->creditsTotal}}" required>
-    </div>
-    <button class="bg-gray-200 hover:bg-blue-900 hover:text-white rounded p-1" type="submit">Update</button>
+    <button class="m-2  border-4 border-yellow-400 hover:text-gray-300 bg-yellow-400 rounded font-bold px-1" type="submit">Update</button>
     @if ($errors->any())
     <div class="alert alert-danger">
         <ul>
