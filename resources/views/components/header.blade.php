376964702c7f7503f99b740d41322ef5b0735f13
idugan100 <isaacd4444@gmail.com>
2024-03-27 09:28:15 -0400

    adding link

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -16,7 +16,7 @@
 </head>
 <body class="bg-gray-200 ">
   <h1 class="text-6xl flex p-4 font-bold bg-yellow-400 text-red-800 border-y-4 justify-center border-black text-center">salisbury analytics <img src="{{ URL::asset('gully-removebg-preview.png') }}" alt="SUlogo" class= "w-16 h-16 hover:animate-pulse ml-8"/></h1>
-  <a href="" target="_blank" class="text-xl flex p-1 font-bold bg-red-800 hover:underline text-yellow-400 border-y-4 justify-center border-black text-center border-t-0">Make your voice heard - vote in the SGA election now &#8594 </a>
+  <a href="https://nam10.safelinks.protection.outlook.com/?url=https%3A%2F%2Fsalisbury.co1.qualtrics.com%2Fjfe%2Fform%2FSV_9uJDW6lY6SgipjU&data=05%7C02%7Csgapres%40gulls.salisbury.edu%7C8bcd6a8450044e94effe08dc4dce831f%7C2472f1faf24f421badd7b01c4b49be07%7C0%7C0%7C638470796565385262%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C0%7C%7C%7C&sdata=Scla2y2dWqLKAKn1xrZy2QuXrQ2Ltvt2MmRIX9jAG5k%3D&reserved=0" target="_blank" class="text-xl flex p-1 font-bold bg-red-800 hover:underline text-yellow-400 border-y-4 justify-center border-black text-center border-t-0">make your voice heard - vote in the SGA election now &#8594 </a>
   <x-navbar></x-navbar>
 
     {{$slot}}
